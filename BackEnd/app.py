8bb343e87b79c4ea49fd62c88d3bf85a7762f4fe
OSCARJONES1999 <jones.oscar.work@outlook.com>
2025-09-01 16:55:20 +0100

    initial commit

---
+++ BackEnd/app.py
@@ -0,0 +1,38 @@
+class BankAccount(object):
+    balance = 0
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"{self.name}'s account has a balance of ${self.balance:.2f}"
+
+    def show_balance(self):
+        print(f"Total balance is ${self.balance:.2f}")
+
+    def deposit(self, amount):
+        if amount <= 0:
+            print("Deposit must be a positive value")
+            return
+        else:
+            print(f"Depositing ${amount:.2f}")
+            self.balance += amount
+            self.show_balance()
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Not enough money in account")
+        else:
+            print(f"Withdrawing ${amount:.2f}")
+
+            self.balance -= amount
+            self.show_balance()
+
+myaccount = BankAccount("Oscar")
+myaccount.deposit(2000)
+myaccount.withdraw(200)
+myaccount.withdraw(3000)
+myaccount.show_balance()
+
+
+
+
+
